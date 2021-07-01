b129eb87500a279099190b5b0778bc4caeeec65a
robdcampbell <rdeancampbell@gmail.com>
2021-06-30 23:25:18 -0400

    Begin Edit modal restyle

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,6 @@
 const { Pool, Client } = require("pg");
 require("dotenv").config();
 
-// const devConfig = {
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-//   database: process.env.PG_DATABASE,
-//   port: process.env.PG_PORT,
-//   host: process.env.PG_HOST,
-// };
-
 const devConfig = {
   connectionString: `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`,
 };
@@ -17,7 +9,7 @@ const proConfig = {
   connectionString: process.env.DATABASE_URL, // coming from Heroku addons
 };
 
-// TEST client AS POOL:
+// RENAME!!!!  POOL to CLIENT:
 const pool = new Client({
   connectionString: process.env.DATABASE_URL,
 });
